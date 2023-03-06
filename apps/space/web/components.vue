21c56b03f94c76064a23a8cfd360f17cb4de92c8
kw.hur <kw.hur@exinno.net>
2023-03-06 15:13:44 +0900

    vite config

---
+++ apps/space/web/components.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { registry } from 'app2-common';
+
+const { uiService } = registry;
+
+uiService.registerComponents('View', {
+  home: () => import('./views/home.vue'),
+  UploadProgress: () => import('./views/UploadProgress.vue')
+});
+
+export default {};
+</script>
